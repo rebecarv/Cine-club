2e793306cf1e74ed85db9f9cc58926bfa5c72a8e
rebecarv <saudeefood@gmail.com>
2025-03-07 14:02:46 -0300

    Fix no-poster

--- project-react/src/components/MovieCard.jsx
+++ project-react/src/components/MovieCard.jsx
@@ -4,7 +4,7 @@ const MovieCard = ({ movie: { title, vote_average, poster_path, release_date, or
     return (
         <div className='movie-card'>
             <img 
-            src={poster_path ? `https://image.tmdb.org/t/p/w500/${poster_path}` : '/no-movie.png'} 
+            src={poster_path ? `https://image.tmdb.org/t/p/w500/${poster_path}` : './no-movie.png'} 
             alt='{title}'
             />
 
