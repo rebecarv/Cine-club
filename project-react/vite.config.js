6bb932927968d36dad8b958e3fe36d3e1a616ae0
rebecarv <saudeefood@gmail.com>
2025-03-06 21:37:22 -0300

    Vite config

--- project-react/vite.config.js
+++ project-react/vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  Base: '/project-react/',
+  Base: '/Cine-club/',
   plugins: [react(), tailwindcss()],
 })
 
